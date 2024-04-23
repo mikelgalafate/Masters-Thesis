ed9048e4b11aefd661a665b26ae3bbf418c9814b
mikelgalafate <mikelgalafate15@gmail.com>
2024-04-23 13:40:34 +0200

    Create the Experiment class and add support to the previously created Parser class.

--- 002-Code/experiment.py
+++ 002-Code/experiment.py
@@ -1,38 +1,30 @@
-import glob
+import os.path
+import sys
 import utils
 import argparse
+from parser import Parser
+
+
+class Experiment(object):
+    def __init__(self):
+        if not len(sys.argv) > 1:
+            utils.error("No arguments given. Use --help for usage information.")
+
+        # Parse parameters
+        self.parameters = Parser().parse_arguments()
+        self.model = None
 
 
 if __name__ == "__main__":
+    # Create experiment object
+    experiment = Experiment()
 
-    argparse = argparse.ArgumentParser()
-    available_architectures = [file.rsplit('/')[-1][:-3] for file in glob.glob('networks/models/*.py')
-                               if 'Model.py'not in file and '__init__' not in file]
-    if len(available_architectures) != 0:
-        mode_group = argparse.add_mutually_exclusive_group(required=True)
-        mode_group.add_argument("-a", "--architecture",
-                                type=str, choices=available_architectures,
-                                default=None,
-                                help=f"Name of the architecture to be used.")
-        mode_group.add_argument("-m", "--model",
-                                type=str,
-                                default=None,
-                                help="Path to saved model")
-    else:
-        argparse.add_argument("-m", "--model",
-                              type=str,
-                              default=None,
-                              help="Path to saved model",
-                              required=True)
-    argparse.add_argument("-v", "--verbose", action="store_true", help="Verbose network summary")
-
-    args = argparse.parse_args()
-    for arg in vars(args):
-        atr = getattr(args, arg)
+    for arg in vars(experiment.parameters):
+        atr = getattr(experiment.parameters, arg)
         print(f"{arg}: {atr}")
 
     # Load model
-    model = utils.load_model(args, input_shape=(512, 512, 3))
+    experiment.model = utils.load_model(experiment.parameters, input_shape=(512, 512, 512))
 
     # Print model summary
     if args.verbose:
